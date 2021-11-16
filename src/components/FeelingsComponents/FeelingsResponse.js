bb6cd7bc245e6ef73017e2d95d8f5acddb68e400
John Dunstan <johntdunstan@gmail.com>
2021-11-16 10:13:54 -0700

    Work on quote API responses

--- src/components/FeelingsComponents/FeelingsResponse.js
+++ src/components/FeelingsComponents/FeelingsResponse.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 function FeelingsResponse(props) {
+  const [quote, setQuote] = useState(null)
+  const [author, setAuthor] = useState(null)
+
+  useEffect(() => {
+    fetch('https://api.quotable.io/random')
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(data.content)
+        setAuthor(data.author)
+        // setQuoteData(data)
+        // setQuoteData({ ...data })
+        console.log(`${data.content} —${data.author}`)
+      })
+  }, [])
+
   return (
     <div>
-      <h3>You feel {props.feeling.toLowerCase()}. You doing okay, buddy?</h3>
+      <h3>You feel {props.feeling.toLowerCase()}.</h3>
+      <figure>
+        <blockquote>{quote}</blockquote>
+        <figcaption>
+          <cite>{author}</cite>
+        </figcaption>
+      </figure>
     </div>
   )
 }
